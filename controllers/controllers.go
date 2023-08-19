75a0dd1ce67e427ebac83eca13e3baa6b0c89387
shivimishra <shivimishra130103@gmail.com>
2023-08-19 08:27:32 +0530

    LOGIN and SIGNUP created

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -1,7 +1,14 @@
 package controllers
 
-import(
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
 
+	"github.com/crystinameth/ecommerce/models"
+	"gopkg.in/mgo.v2/bson"
 )
 
 func HashPassword(password string) string{
@@ -13,11 +20,107 @@ func VerifyPassword(userPassword string, givenPassword string) (bool, string){
 }
 
 func Signup() gin.HandlerFunc {
+	
+	return func(c *gin.Context){
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel()   // This will call cancel() when the main function exits. This ensures that the context is properly canceled, which can signal other parts of your program to clean up and terminate gracefully.
+	    
+		var user models.User
+		if err := c.BindJSON(&user); err != nil {
+			c.JSON{http.StatusBadRequest, gin.H{"error": err.Error()}}
+			return
+		}
 
+		validationErr := Validate.Struct(user)
+		if validationErr != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": validationErr})
+			return
+		}
+
+		count, err := UserCollection.CountDocuments(ctx, bson.M{"email": user.Email})
+		if err != nil {
+			log.Panic(err)
+			c.JSON(http.StatusInternalServiceError, gin.H{"error": err})
+			return
+		}
+
+		if count > 0{
+			c.JSON(http.StatusBadRequest, gin.H{"error":"user already exists"})
+		}
+
+		count, err = UserCollection.CountDocuments(ctx, bson.M{"phone":user.Phone})
+
+		defer cancel()
+		if err != nil {
+			log.Panic(err)
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err})
+			return
+		}
+
+		if count > 0 {
+			c.JSON(http.StatusBadRequest, gin.H{"error":"this phone no. is already in use"})
+			return
+		} 
+		password := HashPassword(*user.Password)
+		user.Password = &password
+
+		user.Created_At, _ = time.Parse(timeRFC3339, time.Now().Format(time.RFC3339))
+		user.Updated_At, _ = time.Parse(timeRFC3339, time.Now().Format(time.RFC3339))
+		user.ID = primitive.NewObjectID()
+		user.User_ID = user.ID.Hex()
+        token, refreshtoken, _ := generate.TokenGenerator(*user.Email, *user.First_Name, *user.Last_Name, user.User_ID)
+		user.Token = &token
+		user.Refresh_Token = &refreshtoken
+		user.UserCart = make([]models.ProductUser, 0)
+		user.Address_Details = make([]models.Address, 0)
+		user.Order_Status = make([]models.Order, 0)
+		_, inserterr := UserCollection.InsertOne(ctx, user)
+		if inserterr != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error":"the user did not get created"})
+			return
+		}
+		defer cancel()
+
+		c.JSON(http.StatusCreated, "successfully signed in!")
+	}
 }
 
 func Login() gin.HandlerFunc {
 
+	return func(c *gin.Context){
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel()
+
+		var user models.User
+		if err := c.BindJSON(&user); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err})
+			return
+		}
+
+		err := UserCollection.FindOne(ctx, bson.M{"email": user.Email}).Decode(&founduser)
+		defer cancel()
+
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "login or password incorrect"})
+			return
+		}
+
+		PasswordIsValid, msg := VerifyPassword(*user.Password, *founduser.Password)
+
+		defer cancel()
+
+		if !PasswordIsValid {
+			c.JSON{http.StatusInternalServerError, gin.H{"error": msg}}
+			fmt.Println(msg)
+			return
+		}
+		token, refreshToken, _ := generate.TokenGenerator(*founderuser.Email, *founderuser.First_Name, *founderuser.Last_Name, founderuser.User_ID)
+		defer cancel()
+
+		generate.UpdateAllTokens(token, refreshToken, founderuser.User_ID)
+
+		c.JSON(http.StatusFound, founduser)
+	}
 }
 
 func ProductViewerAdmin() gin.HandlerFunc{
