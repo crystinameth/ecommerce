d83c892463f4435818ab036b11a8edce94b57a07
shivimishra <shivimishra130103@gmail.com>
2023-08-20 18:49:12 +0530

    Delete address

--- controllers/address.go
+++ controllers/address.go
@@ -1,7 +1,9 @@
 package controllers
 
-import(
+import (
+	"context"
 
+	"github.com/crystinameth/ecommerce/models"
 )
 
 func AddAddress() gin.HandlerFunc{
@@ -17,5 +19,33 @@ func EditWorkAddress() gin.HandlerFunc{
 }
 
 func DeleteAddress() gin.HandlerFunc{
+	return func(c *gin.Context){
+		user_id := c.Query("id")
 
+		if user_id == ""{
+			c.Header("Content-Type", "application/json")
+			c.JSON(http.StatusNotFound, gin.H{"Error":"Invalid Search Index"})
+			c.Abort()
+			return
+		}
+
+		addresses := make([]models.Address, 0)
+		usert_id, err := primitive.ObjectIDFromHex(user_id)
+		if err != nil {
+			c.IndentedJSON(500, "Internal server error")
+		}
+
+		var ctx, cancel = content.WithTimeOut(context.Background(), 100*time.Second)
+		defer cancel()
+		filter := bson.D{primitive.E{key:"_id", Value: usert_id}}
+		update := bson.D{{key:"$set", Value: bson.D{primitive.E{key:"address", Value:addresses}}}}
+		_, err = UserCollection.UpdateOne(ctx, filter, update)
+		if err != nil {
+			c.IndentedJSON(404, "Wrong command")
+			return
+		}
+		defer cancel()
+		ctx.Done()
+		c.IndentedJSON(200, "Successfully Deleted")
+	}
 }
