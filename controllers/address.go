8f35d8aaa81c387f30704ca6268156154244468b
shivimishra <shivimishra130103@gmail.com>
2023-08-22 13:12:23 +0530

    function to add address created

--- controllers/address.go
+++ controllers/address.go
@@ -2,15 +2,74 @@ package controllers
 
 import (
 	"context"
-	"time"
 	"net/http"
+	"time"
+	"fmt"
+	"github.com/crystinameth/ecommerce/models"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
-	"github.com/crystinameth/ecommerce/models"
+	"gopkg.in/mgo.v2/bson"
 )
 
 func AddAddress() gin.HandlerFunc{
 
+	return func(c *gin.Context){
+		user_id := c.Query("id")
+
+		if user_id == "" {
+			c.Header("Content-Type", "application/json")
+			c.JSON(http.StatusNotFound, gin.H{"Error":"Invalid search index"})
+			c.Abort()
+			return
+		}
+
+		address, err := primitive.ObjectIDFromHex(user_id)
+		if err != nil {
+			c.IndentedJSON(500,"Internal Server Error")
+		}
+
+		var addresses models.Address
+
+		addresses.Address_id = primitive.NewObjectID()
+
+		if err = c.BindJSON(&addresses); err != nil {
+			c.IndentedJSON(http.StatusNotAcceptable, err.Error())
+		}
+
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		
+		match_filter := bson.D{{key:"$match", Value: bson.D{primitive.E{key:"_id", Value: address}}}}
+		unwind := bson.D{{key:"$unwind", Value: bson.D{primitive.E{key:"path", Value: "$address"}}}}
+		group := bson.D{{key:"$group", Value: bson.D{primitive.E{key:"_id", Value: "$address_id"},{key:"count", Value:bson.D{primitive.E{Key:"$sum", Value: 1}}}}}}
+		pointcursor, err := UserCollection.Aggregate(ctx,mongo.Pipeline{match_filter, unwind, group})
+	
+		if err != nil{
+			c.IndentedJSON(500,"Internal server error")
+		}
+
+		var addressinfo []bson.M
+		if err = pointcursor.All(ctx, &addressinfo); err != nil{
+			panic(err)
+		}
+
+		var size int32
+		for _, address_no := range addressinfo {
+		count := address_no["count"]
+		size =count.(int32)
+		}
+		if size < 2{
+			filter := bson.D{primitive.E{key: "_id", Value: address}}
+			update := bson.D{{key:"$push", Value: bson.D{primitive.E{key:"address", Value: addresses}}}}
+			_, err := UserCollection.UpdateOne(ctx, filter, update)
+			if err != nil {
+				fmt.Println(err)
+			}
+		} else{
+			c.IndentedJSON(400, "Not Allowed")
+		}
+		defer cancel()
+		ctx.Done()
+	}
 }
 
 func EditHomeAddress() gin.HandlerFunc{
